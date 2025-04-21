9238c20aea72118e9ea6934fda8713668dad149e
Raju Veerla <rajuveerla98@gmail.com>
2025-04-21 11:48:27 +0530

    removed api key

--- end_to_end_AI_Agentic_news-research-writing-system/tools.py
+++ end_to_end_AI_Agentic_news-research-writing-system/tools.py
@@ -7,7 +7,6 @@ from crewai_tools import SerperDevTool
 
 load_dotenv()
 os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
-SERPER_API_KEY = "02d230c82ceb3be1205a79e0169f8e35678d1fe1"
 
 # Initialize the tool for internet searching capabilities
 tool = SerperDevTool()
